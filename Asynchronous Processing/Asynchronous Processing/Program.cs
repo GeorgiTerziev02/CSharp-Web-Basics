c73fb75a3764169f8391ff8668ba42740e821537
Georgi Terziev <terzata2002@abv.bg>
2020-01-24 22:12:53 +0200

    Asynchronous Processing

--- Asynchronous Processing/Asynchronous Processing/Program.cs
+++ Asynchronous Processing/Asynchronous Processing/Program.cs
@@ -1,74 +1,178 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Asynchronous_Processing
 {
     class Program
     {
+        static void Exception()
+        {
+            throw new Exception("eee");
+        }
+
         static void Main(string[] args)
         {
-            Stopwatch sw = Stopwatch.StartNew();
+            //TASK PARALLEL LIBRARY (TPL)
+
+
+
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }).ContinueWith((previousTask) =>
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Console.WriteLine(i);
+                }
             
-            var lockObj = new object();
-            decimal money = 0;
-            int count = 0;
-
-            var thread1 = new Thread(()=>
+            });
+        
+            
+            Task.Run(() =>
             {
-                for (int i = 1; i <= 500000; i++)
+                for (int i = 300; i < 500; i++)
                 {
-                    bool isPrime = true;
-
-                    for (int j = 2; j <= Math.Sqrt(i); j++)
-                    {
-                        if (i % j == 0)
-                        {
-                            isPrime = false;
-                        }
-                    }
-
-                    if (isPrime)
-                    {
-                        lock (lockObj)
-                        {
-                            count++;
-                        }
-                    }
+                    Console.WriteLine(i);
                 }
             });
-            thread1.Start();
 
-            var thread2 = new Thread(()=>
+            while (true)
             {
-                for (int i = 500001; i <= 1000000; i++)
-                {
-                    bool isPrime = true;
+                var line = Console.ReadLine();
+                Console.WriteLine(line.ToUpper());
+            }
+            
 
-                    for (int j = 2; j <= Math.Sqrt(i); j++)
-                    {
-                        if (i % j == 0)
-                        {
-                            isPrime = false;
-                        }
-                    }
+            // CANT CATCH THREAD EXCEPTION
+            //try
+            //{
+            //    var thread1 = new Thread(Exception);
+            //    thread1.Start();
+            //    thread1.Join();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
 
-                    if (isPrime)
-                    {
-                        lock (lockObj)
-                        {
-                            count++;
-                        }
-                    }
-                }
-            });
-            thread2.Start();
 
-            thread1.Join();
-            thread2.Join();
 
-            Console.WriteLine(count);
-            Console.WriteLine(sw.Elapsed);
+            Stopwatch sw = Stopwatch.StartNew();
+
+            // DEAD LOCK
+            //var lockObj1 = new object();
+            //var lockObj2 = new object();
+
+            //var thread1 = new Thread(() =>
+            //{
+            //    lock(lockObj1)
+            //    {
+            //        Thread.Sleep(1000);
+            //        lock (lockObj2)
+            //        {
+
+            //        }
+            //    }
+            //});
+            //var thread2 = new Thread(() =>
+            //{
+            //    lock (lockObj2)
+            //    {
+            //        Thread.Sleep(1000);
+            //        lock (lockObj1)
+            //        {
+
+            //        }
+            //    }
+            //});
+
+            //thread1.Start();
+            //thread2.Start();
+            //thread1.Join();
+            //thread2.Join();
+
+            //var numbers = new ConcurrentQueue<int>(Enumerable.Range(0, 10000).ToList());
+
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    new Thread(() =>
+            //    {
+            //        while(numbers.Count > 0)
+            //        {
+            //            numbers.TryDequeue(out _);
+            //        }
+            //    });
+            //}
+
+            return;
+            
+            //var lockObj = new object();
+            //decimal money = 0;
+            //int count = 0;
+
+            //var thread1 = new Thread(()=>
+            //{
+            //    for (int i = 1; i <= 500000; i++)
+            //    {
+            //        bool isPrime = true;
+
+            //        for (int j = 2; j <= Math.Sqrt(i); j++)
+            //        {
+            //            if (i % j == 0)
+            //            {
+            //                isPrime = false;
+            //            }
+            //        }
+
+            //        if (isPrime)
+            //        {
+            //            lock (lockObj)
+            //            {
+            //                count++;
+            //            }
+            //        }
+            //    }
+            //});
+            //thread1.Start();
+
+            //var thread2 = new Thread(()=>
+            //{
+            //    for (int i = 500001; i <= 1000000; i++)
+            //    {
+            //        bool isPrime = true;
+
+            //        for (int j = 2; j <= Math.Sqrt(i); j++)
+            //        {
+            //            if (i % j == 0)
+            //            {
+            //                isPrime = false;
+            //            }
+            //        }
+
+            //        if (isPrime)
+            //        {
+            //            lock (lockObj)
+            //            {
+            //                count++;
+            //            }
+            //        }
+            //    }
+            //});
+            //thread2.Start();
+
+            //thread1.Join();
+            //thread2.Join();
+
+            //Console.WriteLine(count);
+            //Console.WriteLine(sw.Elapsed);
         }
 
         private static void MyThreadMainMethod()
